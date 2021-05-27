9cfefad8166e38ece790d3379c67d0e0c1124eb7
Ashbjornn <62945196+Ashbjornn@users.noreply.github.com>
2021-05-27 16:40:59 +0200

    modal login

---
+++ main.js
@@ -0,0 +1,20 @@
+
+<script>
+    var modal = document.getElementById("modalLog");
+
+    var btn = document.getElementById("logbtn");
+
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+    modal.style.display = "block";
+}
+    span.onclick = function() {
+    modal.style.display = "none";
+}
+    window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+}
+}
+</script>
